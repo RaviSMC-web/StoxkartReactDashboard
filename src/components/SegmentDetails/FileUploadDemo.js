caa0647d6e83e765a640d818ecdd0e037ae5e7f5
RaviSMC-web <ravianuragi@smcindiaonline.com>
2022-09-23 14:32:52 +0530

    final-stage

--- src/components/SegmentDetails/FileUploadDemo.js
+++ src/components/SegmentDetails/FileUploadDemo.js
@@ -31,36 +31,4 @@ export const FileUploadDemo = () => {
 const uploadOptions = {label: 'Uplaod', icon: 'pi pi-upload', className: 'p-button-success'};
 const cancelOptions = {label: 'Cancel', icon: 'pi pi-times', className: 'p-button-danger'};
 
-    return (
-        <div>
-            <div className="segment_btn">
-        <Button
-          icon="pi pi-file"
-          label="Upload Documents"
-          className="p-button-outlined p-button-sm"
-          onClick={() => onClick("displaySegment")}
-        />
-      </div>
-
-            <Dialog
-        onHide={() => onHide("displaySegment")}
-        visible={displaySegment}
-        style={{ height: "75%", width: "400px" }}
-      >
-                <p className="text-2xl">Segment Proof</p>
-          <p className="text-xs">
-            Upload a copy of the cancelled cheque which shows your Name, Bank
-            account number and IFSC code
-          </p>
-                <div className='seg_pop'>
-                <FileUpload name="demo" uploadLabel='' accept="image/*" maxFileSize={1000000}
-                    // emptyTemplate={<p className="m-0">Drag and drop files to here to upload.</p>} 
-                    
-                    chooseOptions={chooseOptions} uploadOptions={uploadOptions} cancelOptions={cancelOptions}/>
-                    </div>
-                    
-           </Dialog>
-        </div>
-    )
-}
                  
